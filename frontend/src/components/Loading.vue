84bd705d8260038c5b282a62dc9eaa0f46419605
Daniil Kraynov <dykrajno@mts.ru>
2020-06-01 19:30:40 +0300

    [feat] use external loading spinner

--- frontend/src/components/Loading.vue
+++ frontend/src/components/Loading.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="loading">
-        <img class="loading-img" src="https://sun9-64.userapi.com/c206724/v206724527/12ac29/cfcrhaFIKUo.jpg">
+        <hour-glass size="100px"></hour-glass>
         Loading...
     </div>
 </template>
@@ -13,26 +13,14 @@
         align-items: center;
         margin-bottom: 100px;
     }
-
-    @keyframes rotating {
-        from {
-            transform: rotate(0deg);
-        }
-        to {
-            transform: rotate(360deg);
-        }
-    }
-
-    .loading-img {
-        width: 300px;
-        height: 300px;
-        animation: rotating 5s linear infinite;
-        margin-bottom: 100px;
-    }
 </style>
 
 <script>
-    export default {
+    import {HourGlass} from 'vue-loading-spinner';
 
+    export default {
+        components: {
+            HourGlass
+        }
     }
 </script>
